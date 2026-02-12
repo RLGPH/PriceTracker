0bd3b59462584e698d39f6890ffd5c39917d6b6f
Simon Teame <simonteame436@gmail.com>
2026-02-12 10:31:42 +0100

    alot of changes but mainly that i added the websites in a list of dictionaries to loop the websites more clean. Other than that, not much. I used the the class names in the html to find price. It now stores product name, the webshop it came from, price and the time it got it.

--- PriceTracker/PriceTracker.py
+++ PriceTracker/PriceTracker.py
@@ -1,28 +1,56 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = "https://www.computersalg.dk/i/24105172/msi-geforce-rtx-5070-12g-ventus-2x-oc-grafikkort-geforce-rtx-5070-12-gb-gddr7-pci-express-5-0-3-x-displayport-hdmi"
+# websites to track
+#just defined the websites in a list of dictionariess
+websites = [
+    {
+        'name': 'Computersalg.dk',
+        'product_name': 'MSI GeForce RTX 5070 12G VENTUS 2X OC',
+        'url': 'https://www.computersalg.dk/i/24105172/msi-geforce-rtx-5070-12g-ventus-2x-oc-grafikkort-geforce-rtx-5070-12-gb-gddr7-pci-express-5-0-3-x-displayport-hdmi',
+        'method': 'itemprop'
+    },
+    {
+        'name': 'elgiganten.dk',
+        'product_name': 'MSI GeForce RTX 5070 12G VENTUS 2X OC grafikkort',
+        'url': 'https://www.elgiganten.dk/product/gaming/pc-komponenter/grafikkort-gpu/msi-geforce-rtx-5070-12g-ventus-2x-oc-grafikkort/900914?utm_content=all&utm_strategy=pricerunner&utm_source=pricerunner&utm_medium=cpc&utm_campaign=pricerunner',
+        'method': '-mt-[6px] font-headline text-[3.5rem] leading-[3.5rem] justify-self-start'
+    }
+]
 
-print("getting page...")
-
-headers = {"User-Agent": "Mozilla/5.0"}
-soup = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
-
-price = (
-    soup.find(attrs={"itemprop": "price"}) and soup.find(attrs={"itemprop": "price"}).get("content")
-    or soup.find(class_="v-product-details__price") and soup.find(class_="v-product-details__price").text.strip()
-    or soup.find("meta"
-                 , {"property": "product:price:amount"}) and soup.find("meta", {"property": "product:price:amount"}).get("content")
-)
-
-if price:
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print("Price:", price)
-
-    with open("prices.txt", "a") as f:
-        f.write(f"{now} - {price}\n")
-
-    print("saved!")
-else:
-    print("couldn't find price :(")
+for site in websites:
+    print(f"Checking {site['name']}...")
+    
+    page = requests.get(site['url'], headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    price = None
+    
+    
+    if site['method'] == 'itemprop':
+        price_element = soup.find(attrs={'itemprop': 'price'})
+        if price_element:
+            price = price_element.get('content')
+            price = price.strip()
+    
+    #finds the price based on the class name 
+    elif site['method'] == '-mt-[6px] font-headline text-[3.5rem] leading-[3.5rem] justify-self-start':
+        price_element = soup.find(class_='-mt-[6px] font-headline text-[3.5rem] leading-[3.5rem] inc-vat')
+        if price_element:
+            price = price_element.text.strip()
+    
+    if price:
+        current_time = datetime.now()
+        time_string = current_time.strftime('%Y-%m-%d %H:%M:%S')
+        
+        file = open('prices.txt', 'a')
+        message = site['name'] + " - " + site['product_name'] + " - " + price + " - " + time_string + "\n" 
+        file.write(message)
+        file.close()
+        
+        print("Price found: " + price)
+    else:
+        print("Price not found")
+    
+    print("")
