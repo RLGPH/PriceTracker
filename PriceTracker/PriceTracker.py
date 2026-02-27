47b7c9c3a235404b42086f694e7a5db31a2d33e8
Simon <74069961+SImo21c9@users.noreply.github.com>
2026-02-27 09:11:07 +0100

    removed some useless trash. basically resolved some conflicts

--- PriceTracker/PriceTracker.py
+++ PriceTracker/PriceTracker.py
@@ -1,18 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-Simon_NewTest
 from datetime import datetime, timezone
 import schedule
 import time
 
 API_URL = "http://localhost:32777/api/scraper"
-
-from datetime import datetime
-import schedule
-import time
-
-API_URL = "http://localhost:32777/api/scraper"
-
 
 # websites to track
 websites = [
